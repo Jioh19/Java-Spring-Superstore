f1176d588aff1751485b6419847ac2a4ac38aff2
Jioh19 <jioh1982@gmail.com>
2023-08-13 21:50:41 -0400

    add integration testing

--- src/test/java/com/ltp/globalsuperstore/GlobalSuperstoreApplicationTests.java
+++ src/test/java/com/ltp/globalsuperstore/GlobalSuperstoreApplicationTests.java
@@ -1,13 +1,42 @@
 package com.ltp.globalsuperstore;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GlobalSuperstoreApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
+	}
+
+	@Test
+	public void testSuccessfulSubmission() throws Exception {
+
+		RequestBuilder request = MockMvcRequestBuilders.post("/submitItem")
+				.param("category", "Furniture")
+				.param("name", "Bed")
+				.param("price", "1000.00")
+				.param("discount", "200.00")
+				.param("date", "2023-01-10");
+
+		mockMvc.perform(request)
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/inventory"));
 	}
 
 }
--- src/test/java/com/ltp/globalsuperstore/StoreServiceTest.java
+++ src/test/java/com/ltp/globalsuperstore/StoreServiceTest.java
@@ -1,11 +1,13 @@
 package com.ltp.globalsuperstore;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Date;
-import java.text.SimpleDateFormat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,10 +30,20 @@ public class StoreServiceTest {
     @Test
     public void getItemRepoTest() {
         when(storeRepository.getItems()).thenReturn(Arrays.asList(
-            new Item("Furniture", "Table", 1000.00, 100.00, new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-10")),
-            new Item("Furniture", "Chair", 500.00, 50.00, new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-10"))
+            new Item("Furniture", "Table", 1000.00, 100.00, new Date()),
+            new Item("Furniture", "Chair", 500.00, 50.00, new Date())
         ));
 
         List<Item> result = storeService.getItems();
+
+        assertEquals("Furniture", result.get(0).getCategory());
+        assertEquals("Chair", result.get(1).getName());
+    }
+
+    @Test
+    public void addItemTest() {
+        Item item = new Item("Furniture", "Bed", 2000.00, 1500.00, new Date());
+        storeService.submitItem(item);
+        verify(storeRepository, times(1)).addItem(item);
     }
 }
